2ddf7788f9070567760015cca1f984b166429b64
Romain HENRY <romainhenry60@gmail.com>
2023-08-30 11:28:03 +0200

    create initial watchlist model

---
+++ src/main/java/com/filmosaurus/javaLearning/model/WatchList.java
@@ -0,0 +1,26 @@
+package com.filmosaurus.javaLearning.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="watchlist")
+public class WatchList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "movie_id")
+    private Long movieId;
+}
