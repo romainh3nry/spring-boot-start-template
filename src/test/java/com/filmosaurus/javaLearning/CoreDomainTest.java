183ab9dc538ee0a378ccd177d350a458469bcc9f
Romain HENRY <romainhenry60@gmail.com>
2023-09-09 16:36:58 +0200

    add test for setters

--- src/test/java/com/filmosaurus/javaLearning/CoreDomainTest.java
+++ src/test/java/com/filmosaurus/javaLearning/CoreDomainTest.java
@@ -21,8 +21,9 @@ public class CoreDomainTest {
         assertThat(movie.getPlot()).isEqualTo("test_plot");
     
     }
+
     @Test
-    void ToStringShouldBeAlsoTested() {
+    void toStringShouldBeAlsoTested() {
         Movie movie = new Movie(
             "test_title", "test_director", "test_release_date", "test_plot" 
         );
@@ -30,5 +31,24 @@ public class CoreDomainTest {
             .isEqualTo(
                 "Movie{id=null, title='test_title', director='test_director', release_date='test_release_date', plot='test_plot'}"
             );
+    }
+
+    @Test
+    void settersShouldMutateState() {
+        Movie movie = new Movie(
+            "test_title", "test_director", "test_release_date", "test_plot" 
+        );
+
+        movie.setId(99L);
+        movie.setTitle("new_title");
+        movie.setDirector("new_director");
+        movie.setRelease_date("new_release_date");
+        movie.setPlot("new_plot");
+
+        assertThat(movie.getId()).isEqualTo(99L);
+        assertThat(movie.getTitle()).isEqualTo("new_title");
+        assertThat(movie.getDirector()).isEqualTo("new_director");
+        assertThat(movie.getRelease_date()).isEqualTo("new_release_date");
+        assertThat(movie.getPlot()).isEqualTo("new_plot");
     }
 }
